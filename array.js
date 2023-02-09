b4fa6d45d62278d354f160b913280591c8866d20
Kristin Schlossman <Kristin.Schlossman@gmail.com>
2023-02-08 18:00:41 -0800

    Add comment for instructors on boolea arrays. Moves on to rest of project.

---
+++ array.js
@@ -0,0 +1,8 @@
+var guests = ["Adam", "Brian", "Damien", "Ellie"]
+var ages = [28, 34, 29, 37]
+var likesDnD = [true, false, true, true]
+//booleans arrays don't quite make sense to me yet. 
+// I've reviewed documentation and sent pm's to get some help. 
+// while I wait on that I'm going to submit all that I currently understand
+// with the intent of resubbing this. 
+
